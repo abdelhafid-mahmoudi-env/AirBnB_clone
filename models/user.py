de9b2064010790dafa4d40778bfbd468efe12a9a
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-02-10 03:01:29 -0800

    test

--- models/user.py
+++ models/user.py
@@ -1,12 +1,19 @@
+#!/usr/bin/python3
+"""Module containing the User class"""
+
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    """User class that inherits from BaseModel"""
+    """
+    User class that inherits from BaseModel class.
+    Attributes:
+        email (str): The email address of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes User"""
-        super().__init__(*args, **kwargs)
