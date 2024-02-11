c284539a000a17f8a28d79ec13bb967c81933798
EL ORCHE <amineelorche@gmail.com>
2024-02-11 08:01:35 -0800

    script added

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 Defines the BaseModel class that serves as a base for all other classes
 in our project.
 """
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 
@@ -15,40 +15,42 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
-        If kwargs is not empty, initializes with key-value pairs in kwargs,
-        converting datetime strings to datetime objects for created_at and
-        updated_at. Otherwise, initializes with a new UUID and the current
-        datetime for created_at and updated_at.
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(value)
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != '__class__':
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            from models import storage
             storage.new(self)
 
     def __str__(self):
         """
         Returns a string representation of the BaseModel instance.
         """
-        cls_name = self.__class__.__name__
-        return f"[{cls_name}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates 'updated_at' with the current datetime and saves to file."""
+        """
+        Updates 'updated_at' with the current datetime, and saves the instance
+        to the file storage.
+        """
         self.updated_at = datetime.now()
+        from models import storage
         storage.new(self)
         storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary of the instance's __dict__.
+        Returns a dictionary of the instance's __dict__, with objects
+        converted to appropriate representation for JSON serialization,
+        including the class name in the '__class__' key.
         """
-        my_dict = self.__dict__.copy()
+        my_dict = dict(self.__dict__)
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
