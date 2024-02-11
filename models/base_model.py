82e148a4e852850290e4041a9eeeed5f2ae549c6
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-02-11 13:25:32 -0800

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-""" BaseModel Classe """
 
 from uuid import uuid4
 from datetime import datetime
-
 
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
     def __init__(self, *args, **kwargs):
         """Initializes a new instance of BaseModel."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        value = datetime.fromisoformat(value)
-                    setattr(self, key, value)
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs and (kwargs is not None) and (kwargs != {}):
+            for attr, value in kwargs.items():
+                if attr == "__class__":
+                    continue
+                elif attr == "created_at" or attr == "updated_at":
+                    new_attr_value = datetime.fromisoformat(kwargs[attr])
+                    setattr(self, attr, new_attr_value)
+                else:
+                    setattr(self, attr, value)
+            storage.new(self)
         else:
-            self.id = str(uuid4())
-            self.created_at = self.updated_at = datetime.now()
             from models import storage
             storage.new(self)
 
@@ -44,10 +47,8 @@ class BaseModel:
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values."""
-        my_dict = {
-            key: value.isoformat()
-            if isinstance(value, datetime)
-            else value for key, value in self.__dict__.items()
-        }
-        my_dict['__class__'] = self.__class__.__name__
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
