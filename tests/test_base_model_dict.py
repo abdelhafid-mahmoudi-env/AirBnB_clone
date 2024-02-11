e6a68452e2e96d2911d01c255a44a577d1d9c540
EL ORCHE <amineelorche@gmail.com>
2024-02-11 06:15:09 -0800

    script added

--- tests/test_base_model_dict.py
+++ tests/test_base_model_dict.py
@@ -8,7 +8,6 @@ print(my_model.id)
 print(my_model)
 print(type(my_model.created_at))
 print("--")
-
 my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
@@ -16,7 +15,6 @@ for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
 
 print("--")
-
 my_new_model = BaseModel(**my_model_json)
 print(my_new_model.id)
 print(my_new_model)
