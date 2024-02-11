52298febefd2afb2c0cab8fab45c6207df8404ba
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-02-10 16:03:32 -0800

    test

--- models/base_model.py
+++ models/base_model.py
@@ -15,27 +15,27 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """
-        Initializes a new BaseModel instance.
+        Initializes a new instance of BaseModel.
         """
         self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
         for key, value in kwargs.items():
-            if key == '__class__':
-                continue
-            if key in ['created_at', 'updated_at']:
-                value = datetime.fromisoformat(value)
-            setattr(self, key, value)
+            if key != '__class__':
+                if key in ('created_at', 'updated_at'):
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
 
     def __str__(self):
         """
         Returns a string representation of the BaseModel instance.
         """
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
-        Updates the public instance attribute 'updated_at' with the current datetime.
+        Updates the updated_at attribute with the current datetime.
         """
         self.updated_at = datetime.now()
 
@@ -43,8 +43,8 @@ class BaseModel:
         """
         Returns a dictionary containing all keys/values of the instance.
         """
-        dictionary = dict(self.__dict__)
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        dict_rep = self.__dict__.copy()
+        dict_rep['__class__'] = self.__class__.__name__
+        dict_rep['created_at'] = self.created_at.isoformat()
+        dict_rep['updated_at'] = self.updated_at.isoformat()
+        return dict_rep
