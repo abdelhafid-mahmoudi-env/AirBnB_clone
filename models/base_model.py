4cb3f73c8ce256c8f8f1cc8369c528c8360dd51a
EL ORCHE <amineelorche@gmail.com>
2024-02-11 07:33:29 -0800

    script added

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """
-Defines the BaseModel class that serves as a base for all other classes in our project.
+Defines the BaseModel class that serves as a base for all other classes
+in our project.
 """
 from uuid import uuid4
 from datetime import datetime
+
 
 class BaseModel:
     """
@@ -19,32 +21,31 @@ class BaseModel:
             self.created_at = self.updated_at = datetime.now()
         else:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
+                if key in ['created_at', 'updated_at']:
                     value = datetime.fromisoformat(value)
                 if key != '__class__':
                     setattr(self, key, value)
-            if 'id' not in kwargs:
-                self.id = str(uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = self.created_at
+            self.id = kwargs.get('id', str(uuid4()))
+            now = datetime.now()
+            self.created_at = kwargs.get('created_at', now)
+            self.updated_at = kwargs.get('updated_at', now)
 
     def __str__(self):
         """
         Returns a string representation of the BaseModel instance.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        cls_name = self.__class__.__name__
+        return f"[{cls_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
-        Updates the 'updated_at' attribute with the current datetime.
+        Updates 'updated_at' with the current datetime.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of the instance's __dict__.
+        Returns a dictionary of the instance's __dict__.
         """
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
