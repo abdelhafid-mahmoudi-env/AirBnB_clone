80fbabc0208c785fbea4ea9118bd1379067e436e
EL ORCHE <amineelorche@gmail.com>
2024-02-11 06:56:50 -0800

    script added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,49 +1,34 @@
 #!/usr/bin/python3
 """
-Serializes instances to a JSON file & deserializes JSON file to instances
+Defines class FileStorage
 """
 import json
 from models.base_model import BaseModel
 
-
 class FileStorage:
-    """
-    serializes instances to a JSON file and deserializes JSON file to instances
-    """
+    """Serializes instances to a JSON file
+    and deserializes JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns the dictionary '__objects'
-        """
+        """Returns the dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Sets in '__objects' the obj with key <obj class name>.id
-        """
+        """Sets in __objects the obj with key <obj class name>.id"""
         if obj:
-            key = obj.__class__.__name__ + '.' + obj.id
+            key = f"{obj.__class__.__name__}.{obj.id}"
             self.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes '__objects' to the JSON file (path: '__file_path')
-        """
-        obj_dict = {}
-        for obj in self.__objects.keys():
-            obj_dict[obj] = self.__objects[obj].to_dict()
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {obj: self.__objects[obj].to_dict() for obj in self.__objects}
         with open(self.__file_path, 'w') as f:
-            json.dump(obj_dict, f, indent=4)
+            json.dump(obj_dict, f)
 
     def reload(self):
-        """
-        Deserializes JSON file to '__objects'
-        (only if the JSON file ('__file_path') exists;
-        otherwise, do nothing. If the file doesn’t exist,
-        no exception should be raised)
-        """
+        """Deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as f:
                 obj_dict = json.load(f)
