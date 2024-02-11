cd2e51b51d7ae2f64e8b7a57105667433a57bef0
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-02-11 12:11:25 -0800

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import json
+"""file serialization-deserialization"""
+import json
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -17,7 +19,7 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects."""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
@@ -26,21 +28,20 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
-        obj_dict = {
-            obj: self.__objects[obj].to_dict()
-            for obj in self.__objects.keys()
-        }
-        with open(self.__file_path, 'w') as f:
-            json.dump(obj_dict, f)
+        dict_items = {}
+        for key, value in self.__objects.items():
+            dict_items[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
+            json.dump(dict_items, f)
 
     def reload(self):
         """Deserializes the JSON file to __objects, if it exists."""
         try:
             with open(self.__file_path, 'r') as f:
                 obj_dict = json.load(f)
-            for obj_id, obj_attrs in obj_dict.items():
-                cls_name = obj_attrs["__class__"]
-                cls = eval(cls_name)
-                self.__objects[obj_id] = cls(**obj_attrs)
+                for obj_id, obj_attrs in obj_dict.items():
+                    cls_name = obj_attrs["__class__"]
+                    cls = eval(cls_name)
+                    self.__objects[obj_id] = cls(**obj_attrs)
         except FileNotFoundError:
             pass
