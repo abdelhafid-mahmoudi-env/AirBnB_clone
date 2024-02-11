91d3a9da68d712be9e72b82a9d06df7a489fbaeb
EL ORCHE <amineelorche@gmail.com>
2024-02-11 07:11:03 -0800

    script added

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,11 @@ Initializes the models package
 """
 from models.engine.file_storage import FileStorage
 
-
+class_registry = {
+    'BaseModel': BaseModel,
+    'User': User,
+    # Ajoutez d'autres classes au besoin
+}
 # Create an instance of FileStorage
 storage = FileStorage()
 # Reload objects from file to __objects dictionary, if file exists
